aa2460e0ae2362a1c85b648585c4ccb81ac3619c
haibara25 <mariemamdiuni125@gmail.com>
2025-04-16 18:32:25 +0100

    Button

---
+++ Button.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Button: React.FC = () => {
+  return (
+    <div className="bg-blue-600 p-2 m-3 h-11 w-28 text-white text-center rounded-md hover:text-blue-600 hover:bg-slate-50">
+      Click Here
+    </div>
+  );
+};
+
+export default Button;

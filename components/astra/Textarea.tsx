55a5ca72b3c8cf0db27605380f724bb1d7a1aaaa
Ege Uysal <hello@egeuysal.com>
2025-04-18 20:47:47 -0500

    Update Textarea.tsx

--- components/astra/Textarea.tsx
+++ components/astra/Textarea.tsx
@@ -57,7 +57,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
         defaultValue={defaultValue}
         onChange={onChange}
         className={cn(
-          "text-sm md:text-base tracking-tight w-full flex flex-col py-2.5 px-3.5 rounded-lg placeholder:text-black/50 dark:placeholder:text-white/50 text-black dark:text-white caret-black dark:caret-white border border-[#f2f2f2] dark:border-[#2e2e2e] h-24 outline-none transition-colors focus:ring-2 focus:ring-primary-500 disabled:opacity-60 disabled:cursor-not-allowed focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[1px]",
+          "text-sm md:text-base tracking-tight w-full flex flex-col py-2.5 px-3.5 rounded-lg placeholder:text-black/50 dark:placeholder:text-white/50 text-black dark:text-white caret-black dark:caret-white border border-neutral-200 dark:border-neutral-800  h-24 outline-none transition-colors focus:ring-2 focus:ring-primary-500 disabled:opacity-60 disabled:cursor-not-allowed focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[1px]",
           className
         )}
       />
